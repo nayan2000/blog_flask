8eaf762c5119b83eb25eeec8be793e8509932a61
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-06 20:47:00 +0530

    users signup, login and logout implemented

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,17 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_login import LoginManager 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 # Database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+#Login
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
