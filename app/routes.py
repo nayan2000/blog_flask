42989dba41298f501d21162a94984fa8c3aa57c8
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-06 07:17:53 +0530

    Templates done and Forms in progress

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'Nayan'}
+    posts = [
+        {
+            'author': {'username' : 'John'},
+            'body': 'I am so cooool'
+        },
+        {
+            'author': {'username' : 'Nayan'},
+            'body': 'The Glass is full of water.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+#Jinja2 templates preloaded in flask
