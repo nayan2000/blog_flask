40aaa83576c60042aea4163fcfb735f70cae71bb
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-07 06:36:26 +0530

    Custom and beautified Error pages; Email Reporting for any failure of application added

--- config.py
+++ config.py
@@ -9,4 +9,10 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    
+    #email configuration for error_reporting
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['nayan1848@gmail.com']
