e7573533b866cf39daca7997a34f137890da74aa
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-09 04:51:19 +0530

    Followers/following models, db set up

--- app/models.py
+++ app/models.py
@@ -10,6 +10,13 @@ from hashlib import md5
 def load_user(id):
     return User.query.get(int(id))
 
+#defining db relations for many-to-may-field for followers feature
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -18,6 +25,17 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    #followers : Many to Many 
+    # self referential relation on User (User to User)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy='dynamic'), 
+        lazy = 'dynamic'
+    )
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
