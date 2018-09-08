3a3fa6994dfe8026fbd58a25bbd2885008d67319
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-09 05:12:39 +0530

    User feeds created and optimized queries

--- app/models.py
+++ app/models.py
@@ -65,7 +65,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count()>0
 
-    
+    def followed_posts(self):
+        followed_posts = Post.query.join(
+            followers, 
+            (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own_posts = Post.query.filter_by(user_id=self.id)
+        return followed_posts.union(own_posts).order_by(Post.timestamp.desc())
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
