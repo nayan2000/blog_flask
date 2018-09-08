f7ace7e31a81a36e1cf6814c74ee8c960d3c5988
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-09 04:16:40 +0530

    Error logging setup, emails on error done and duplicate username bug fixed with optimisation of db requests;

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,7 @@ login.login_view = 'login'
 
 from app import routes, models, errors
 
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -31,10 +32,26 @@ if not app.debug:
         if app.config['MAIL_USE_TLS']:
             secure = ()
         mail_handler = SMTPHandler(
-            mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr = 'no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs = app.config['ADMINS'], subject = 'Microblog failure',
-            credentials=auth, secure=secure
-        )
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+from logging.handlers import RotatingFileHandler
+import os
+
+if not app.debug:
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount = 10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
