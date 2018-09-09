f30546d32e78924644aed3e0cff5826c31c57e36
Nayan Khanna <f20170636@pilani.bits-pilani.ac.in>
2018-09-09 05:40:35 +0530

    UNIT TESTING: user feeds tests written;

--- app/tests.py
+++ app/tests.py
@@ -50,5 +50,47 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(u1.followed.count(), 0)
         self.assertTrue(u2.followers.count(), 0)
 
+    def test_follow_posts(self):
+        u1 = User(username = 'john', email='john1@gmail.com')
+        u2 = User(username = 'nayan', email='nayan1@gmail.com')
+        u3 = User(username = 'hello', email='hello1@gmail.com')
+        u4 = User(username = 'broo', email='broo1@gmail.com')
+
+        db.session.add_all([u1, u2, u3, u4])
+
+        now = datetime.utcnow()
+        p1 = Post(
+            body='post from john', author=u1, timestamp=now + timedelta(seconds=1)
+        )
+        p2 = Post(
+            body='post from nayan', author=u2, timestamp=now + timedelta(seconds=4)
+        )
+        p3 = Post(
+            body='post from hello', author=u3, timestamp=now + timedelta(seconds=3)
+        )
+        p4 = Post(
+            body='post from broo', author=u4, timestamp=now + timedelta(seconds=2)
+        )
+
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
+
+        u1.follow(u2)
+        u1.follow(u4)
+        u2.follow(u3)
+        u3.follow(u4)
+        
+        db.session.commit()
+
+        f1 = u1.followed_posts.all()
+        f2 = u2.followed_posts.all()
+        f3 = u3.followed_posts.all()
+        f4 = u4.followed_posts.all()
+
+        self.assertEqual(f1, [p2, p4, p1])
+        self.assertEqual(f2, [p2, p3])
+        self.assertEqual(f3, [p3, p4])
+        self.assertEqual(f4, [p4])
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
